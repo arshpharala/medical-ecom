2f1d6dd52e8a619f518791e21204e4834f961244
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-12-17 18:47:40 +0400

    blogs

--- resources/views/theme/oms/components/news.blade.php
+++ resources/views/theme/oms/components/news.blade.php
@@ -13,18 +13,17 @@
             <div class="col-xl-4 col-lg-4 col-md-6">
               <div class="blog-wrapper mb-30">
                 <div class="blog-img pos-rel">
-                  <a href="blog-details.html"><img src="theme/medibazaar/assets/img/blog/01.jpg" alt=""></a>
-                  <span class="blog-tag color-1">covid -19</span>
+                  <a href="{{ route('news.show', ['news' => $n->slug]) }}"><img src="{{ $n->thumbnail ? asset('storage/' . $n->thumbnail) : asset('assets/img/blog/001.jpg') }}" alt=""></a>
+                  <span class="blog-tag color-1">{{ $n->category->translation->name }}</span>
                 </div>
                 <div class="blog-text">
                   <div class="blog-meta">
-                    <span><i class="far fa-calendar-alt"></i> <a href="blog-details.html">25 Aug
-                        2020</a></span>
+                    <span><i class="far fa-calendar-alt"></i> <a href="{{ route('news.show', ['news' => $n->slug]) }}">{{ $n->published_at ? $n->published_at->format('d M Y') : $n->created_at->format('d M Y') }}</a></span>
                   </div>
-                  <h4><a href="blog-details.html">Mirage Deep Dive Under anding Timin Response</a></h4>
-                  <p>Sedut perspiciatis unde omnis natus error sit voluptatem accusantium </p>
+                  <h4><a href="{{ route('news.show', ['news' => $n->slug]) }}">{{ $n->translation->title }}</a></h4>
+                  <p>{!! $n->translation->intro !!} </p>
                   <div class="b-button gray-b-button">
-                    <a href="blog-details.html">read more <i class="far fa-plus"></i></a>
+                    <a href="{{ route('news.show', ['news' => $n->slug]) }}">read more <i class="far fa-plus"></i></a>
                   </div>
                 </div>
               </div>
